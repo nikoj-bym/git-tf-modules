40794a6d3962f06f6cf33e87dc03fcc6e12868ee
Nikolai <nikolai.johannessen@bym.oslo.kommune.no>
2022-08-31 08:48:35 +0200

    removed comment

--- modules/myec2/myec2.tf
+++ modules/myec2/myec2.tf
@@ -1,8 +1,3 @@
-# variable "instance_name" {
-#     type = string
-#     default = "njtest-ec2"
-# }
-
 resource "aws_instance" "njtest-ec2" {
   ami           = "ami-089950bc622d39ed8" # Changed to correct region AMI
   instance_type = "t2.micro"
